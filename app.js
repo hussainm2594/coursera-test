4b6ca12e07a6b668994ebcc2ba108b0997b985f4
hussainm2594 <32308198+hussainm2594@users.noreply.github.com>
2017-10-23 08:35:03 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,42 +1,83 @@
-(function(){
-  'use strict'
-  angular.module('myFirstAssignment',[])
-  .controller('AssgnController',function($scope){
+(function () { /* always start with these two lines */
+'use strict'; /* prevents bleeding into scope */
 
-      $scope.input="";
-      $scope.message="";
-      $scope.totalValue=0;
+angular.module('ShoppingListCheckOff', [])
+.controller('ShoppingListCheckOffController', ShoppingListCheckOffController)
+.controller('list1Controller', list1Controller)
+.controller('list2Controller', list2Controller)
+.service('ShoppingListCheckOffService', ShoppingListCheckOffService);
 
-      $scope.displayMessage=function(){
-        if ($scope.input === "" || $scope.input === undefined
-              || $scope.input.length === 0)
-          {
-              $scope.message =  "Please enter food first!";
+list1Controller.$inject = ['$scope', 'ShoppingListCheckOffService']; /* first controller */
+function list1Controller($scope, ShoppingListCheckOffService, AlreadyBoughtController) {
+  var ToBuyController = this;
+  $scope.ToBuyController = ToBuyController;
+  $scope.ToBuyController = ShoppingListCheckOffService.populateItems();
+  $scope.remove = function(index) {
+    ShoppingListCheckOffService.remove(index);
+  };
+}
 
-          }
-          else{
-      var totalmessage=calculateMessage($scope.input);
-      $scope.message=totalmessage;
-    }
-    };
-    function calculateMessage(string)
+
+list2Controller.$inject = ['$scope', 'ShoppingListCheckOffService']; /* second controller */
+function list2Controller($scope, ShoppingListCheckOffService, ToBuyController) {
+  var AlreadyBoughtController = this;
+  $scope.AlreadyBoughtController = AlreadyBoughtController;
+  $scope.AlreadyBoughtController = ShoppingListCheckOffService.emptyStart();
+}
+/* inject to protect from minification */
+ShoppingListCheckOffController.$inject = ['$scope', 'ShoppingListCheckOffService'];
+function ShoppingListCheckOffController($scope) { /* dollar sign reserved for angularJS */
+
+};
+
+function ShoppingListCheckOffService() {
+  var service = this;
+  var array1 = this; /* controller as syntax */
+  array1 = [
     {
-      var m1="";
-
-
-      var arrayOfStrings=string.split(",");
-      console.log(arrayOfStrings);
-      var length=arrayOfStrings.length;
-
-      if(length<=3){
-        m1="Enjoy!";
-      }
-      if(length>3){
-        m1="too Much"
-      }
-
-      return m1;
+      name: "Chicken Nuggets",
+      quantity: "200"
+    },
+    {
+      name: "Biscuits",
+      quantity: "50"
+    },
+    {
+      name: "Ice Cream Sandwiches",
+      quantity: "100"
+    },
+    {
+      name: "Hot Dogs",
+      quantity: "10"
+    },
+    {
+      name: "Cakes",
+      quantity: "20"
     }
+  ]; //empty array
+  // $scope.ToBuyController = ToBuyController; /* add to scope */
+  var array2 = this; /* controller as syntax */
+  array2 = []; /* empty array */
 
-  });
+  service.populateItems = function () {
+    return array1;
+  };
+
+  service.emptyStart = function () {
+    return array2;
+  }
+
+  service.addItem = function (array2) { //add an item function
+  // for(var i=0; i<array.length, i++){
+  //   items.push(array(i)); //push this new item variable into the items array
+  // }
+  };
+  service.remove = function(index) {
+      var i = array1[index]; //pull the value and place in a variable
+      array1.splice(index, 1); //remove the item from the 1st array
+      array2.push(i); //push into the 2nd array
+
+    };
+};
+
 })();
