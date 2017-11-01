a382e37dc04c08ac309a884db20c5aab20a5b07b
hussainm2594 <32308198+hussainm2594@users.noreply.github.com>
2017-11-01 04:23:09 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,83 +1,83 @@
-(function () { /* always start with these two lines */
-'use strict'; /* prevents bleeding into scope */
+(function() {
+  'use strict';
+  angular
+    .module('NarrowItDownApp', [])
+    .controller('NarrowItDownController', NarrowItDownController)
+    .service('MenuSearchService', MenuSearchService)
+    .directive('foundItems', FoundItemsDirective);
 
-angular.module('ShoppingListCheckOff', [])
-.controller('ShoppingListCheckOffController', ShoppingListCheckOffController)
-.controller('list1Controller', list1Controller)
-.controller('list2Controller', list2Controller)
-.service('ShoppingListCheckOffService', ShoppingListCheckOffService);
-
-list1Controller.$inject = ['$scope', 'ShoppingListCheckOffService']; /* first controller */
-function list1Controller($scope, ShoppingListCheckOffService, AlreadyBoughtController) {
-  var ToBuyController = this;
-  $scope.ToBuyController = ToBuyController;
-  $scope.ToBuyController = ShoppingListCheckOffService.populateItems();
-  $scope.remove = function(index) {
-    ShoppingListCheckOffService.remove(index);
-  };
-}
-
-
-list2Controller.$inject = ['$scope', 'ShoppingListCheckOffService']; /* second controller */
-function list2Controller($scope, ShoppingListCheckOffService, ToBuyController) {
-  var AlreadyBoughtController = this;
-  $scope.AlreadyBoughtController = AlreadyBoughtController;
-  $scope.AlreadyBoughtController = ShoppingListCheckOffService.emptyStart();
-}
-/* inject to protect from minification */
-ShoppingListCheckOffController.$inject = ['$scope', 'ShoppingListCheckOffService'];
-function ShoppingListCheckOffController($scope) { /* dollar sign reserved for angularJS */
-
-};
-
-function ShoppingListCheckOffService() {
-  var service = this;
-  var array1 = this; /* controller as syntax */
-  array1 = [
-    {
-      name: "Chicken Nuggets",
-      quantity: "200"
-    },
-    {
-      name: "Biscuits",
-      quantity: "50"
-    },
-    {
-      name: "Ice Cream Sandwiches",
-      quantity: "100"
-    },
-    {
-      name: "Hot Dogs",
-      quantity: "10"
-    },
-    {
-      name: "Cakes",
-      quantity: "20"
-    }
-  ]; //empty array
-  // $scope.ToBuyController = ToBuyController; /* add to scope */
-  var array2 = this; /* controller as syntax */
-  array2 = []; /* empty array */
-
-  service.populateItems = function () {
-    return array1;
-  };
-
-  service.emptyStart = function () {
-    return array2;
+  function FoundItemsDirective() {
+    var ddo = {
+      templateUrl: 'foundItems.html',
+      scope: {
+        found: '<',
+        onRemove: '&'
+      },
+      controller: FoundItemsDirectiveController,
+      controllerAs: 'list',
+      bindToController: true
+    };
+    return ddo;
   }
 
-  service.addItem = function (array2) { //add an item function
-  // for(var i=0; i<array.length, i++){
-  //   items.push(array(i)); //push this new item variable into the items array
-  // }
-  };
-  service.remove = function(index) {
-      var i = array1[index]; //pull the value and place in a variable
-      array1.splice(index, 1); //remove the item from the 1st array
-      array2.push(i); //push into the 2nd array
+  function FoundItemsDirectiveController() {
+    var list = this;
 
+    list.isEmpty = function() {
+      return list.found != undefined && list.found.length === 0;
+    }
+  }
+
+  NarrowItDownController.$inject = ['MenuSearchService'];
+  function NarrowItDownController(MenuSearchService) {
+    var controller = this;
+
+    controller.searchTerm = "";
+
+    controller.narrowIt = function() {
+      if (controller.searchTerm === "") {
+        controller.items = [];
+        return;
+      }
+      var promise = MenuSearchService.getMatchedMenuItems(controller.searchTerm);
+      promise.then(function(response) {
+        controller.items = response;
+      })
+      .catch(function(error) {
+        console.log("Something went wrong", error);
+      });
     };
-};
 
-})();
+    controller.removeItem = function(index) {
+      controller.items.splice(index, 1);
+    };
+  }
+
+  MenuSearchService.$inject = ['$http'];
+  function MenuSearchService($http) {
+    var service = this;
+
+    service.getMatchedMenuItems = function(searchTerm) {
+        return $http({
+          method: 'GET',
+          url: 'https://davids-restaurant.herokuapp.com/menu_items.json'
+        }).then(function (result) {
+        // process result and only keep items that match
+        var items = result.data.menu_items;
+
+        var foundItems = [];
+
+        for (var i = 0; i < items.length; i++) {
+          if (items[i].description.toLowerCase().indexOf(searchTerm.toLowerCase()) >= 0) {
+            foundItems.push(items[i]);
+          }
+        }
+
+        // return processed items
+        return foundItems;
+      });
+    };
+  }
+
+}
+)();
